096f3dee0e497491bc79d04bcb77c63a64d9a2af
Mees Goudbeek <meesgoudbeek@protonmail.com>
2022-03-09 11:57:09 +0100

    Changed router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CarDetail from "./components/CarDetail";
 import "./styles/main.scss";
